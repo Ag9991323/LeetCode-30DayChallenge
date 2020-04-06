46766a939028d73f70d13c14d30d52f06654a8ca
AShu <ag9991323@gmail.com>
2020-04-06 16:16:36 +0530

    Adding solution of GroupAnagram problem

---
+++ GroupAnagram.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+public class GroupAnagram {
+   
+        public static List<List<String>> groupAnagrams(String[] arr) {
+            List<List<String>> finallst = new ArrayList<>();
+            
+            for(int i=0;i<arr.length; i++) {
+                if(arr[i]!="-1"){
+                    List<String> lst = new ArrayList<String>();
+    
+                    String temp=arr[i];
+                    lst.add(temp);
+                    int hash1=0;
+                    for(int j=0;j<temp.length();j++){
+                        hash1+=temp.charAt(j);
+                        hash1%=100000007;
+                    }
+                    arr[i]="-1";
+                    int count[]= new int[26];
+                    for(int j=0;j<temp.length();j++){
+                        count[temp.charAt(j)-97]+=1;
+                    }
+                    for(int k=i+1;k<arr.length;k++){
+                        if(arr[k]!="-1"){
+                        String temp2=arr[k];
+                        if(temp.length()==temp2.length()){
+                             int hash2=0;
+                            for(int j=0;j<temp2.length();j++){
+                                hash2+=temp2.charAt(j);
+                                hash2%=100000007;
+                            }
+                            if(hash1==hash2){
+                                
+                                    int count2[]= new int[256];
+                                 for(int j=0;j<temp2.length();j++){
+                                    count2[temp2.charAt(j)-97]+=1;
+                                  }
+    
+                                  boolean isequal =true;
+                                  for(int j=0;j<26;j++){
+                                      if(count[j]!=count2[j]){
+                                          isequal=false;
+                                        break;
+                                      }
+                                  }
+    
+                                  if(isequal){
+                                      arr[k]="-1";
+                                    lst.add(temp2);
+    
+                                  }
+                                }
+                   
+                    
+                            }
+    
+                             
+                
+        
+                        }
+        
+                    }
+                    finallst.add(lst);
+        
+                }
+                }
+                
+            return finallst;
+            
+        }
+    
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        String arr[] = new String[n];
+        for(int i=0;i<n;i++){
+            arr[i]= scan.next();
+        }
+      List ls = groupAnagrams(arr);
+        System.out.println(ls);
+    
+
+        }
+    }
--- MoveZeroes.java
+++ MoveZeroes.java
@@ -1,30 +1,48 @@
+import java.io.*;
 import java.util.*;
 
 public class MoveZeroes {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int arr[] = new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]= scan.nextInt();
-        }
-        int zerocount=0;
-        int j=0;
-        for(int i=0;i<n;i++){
-            if(arr[i]==0){
-                zerocount+=1;
-            }
-            else{
-                arr[j++]=arr[i];
-            }
+    // public static void main(String[] args) {
+    //     Scanner scan = new Scanner(System.in);
+    //     int n = scan.nextInt();
+    //     int arr[] = new int[n];
+    //     for(int i=0;i<n;i++){
+    //         arr[i]= scan.nextInt();
+    //     }
+    //     int zerocount=0;
+    //     int j=0;
+    //     for(int i=0;i<n;i++){
+    //         if(arr[i]==0){
+    //             zerocount+=1;
+    //         }
+    //         else{
+    //             arr[j++]=arr[i];
+    //         }
 
-        }
-        for(;j<n;j++){
-            arr[j]=0;
-            zerocount--;
-        }
-        System.out.println(Arrays.toString(arr));
+    //     }
+    //     for(;j<n;j++){
+    //         arr[j]=0;
+    //         zerocount--;
+    //     }
+    //     System.out.println(Arrays.toString(arr));
 
+    //     }
+    // }
+
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        while(T-->0){
+           // OddNo n = new OddNo();
+            int N = Integer.parseInt(br.readLine().trim()); 
+            int[] arr = new int[N];
+            StringTokenizer  s = new StringTokenizer(br.readLine());
+            for(int i=0;i<N;i++){
+                arr[i]=Integer.parseInt(s.nextToken());
+            }
+            //n.oddnoprint(arr, N);
+            System.out.println();
         }
     }
+}
